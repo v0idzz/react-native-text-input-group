d37bd605fa2f018ec531c3ebbd2932c89b000d0c
Piotr Romanowski <wsrh456@gmail.com>
2021-08-15 11:33:30 +0200

    feat: create example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,40 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 import TextInputGroup from 'react-native-text-input-group';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    TextInputGroup.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View style={styles.root}>
+      <View style={styles.container}>
+        <TextInputGroup>
+          <TextInput style={styles.input} placeholder="First name" />
+          <TextInput style={styles.input} placeholder="Last name" />
+          <TextInput
+            style={styles.input}
+            placeholder="Email Address"
+            textContentType="emailAddress"
+            autoCompleteType="email"
+            autoCapitalize="none"
+          />
+        </TextInputGroup>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  container: {
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  input: {
+    backgroundColor: '#e3e3e3',
+    padding: 8,
+    marginVertical: 8,
   },
 });
